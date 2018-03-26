dc89164415a7fee7fe3ff7485144716921475475
Trenten Norwat <tnorwat@gmail.com>
2018-03-26 15:21:43 -0600

    not working damage start

--- src/net/byteplex/ByteplexMatches/teams/Damage.java
+++ src/net/byteplex/ByteplexMatches/teams/Damage.java
@@ -2,26 +2,33 @@ package net.byteplex.ByteplexMatches.teams;
 
 import net.byteplex.ByteplexCore.util.ChatFormat;
 import net.byteplex.ByteplexCore.util.ChatLevel;
-import net.byteplex.ByteplexMatches.ByteplexMatches;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
+import static net.byteplex.ByteplexMatches.ByteplexMatches.blueTeam;
+import static net.byteplex.ByteplexMatches.ByteplexMatches.redTeam;
+
 public class Damage implements Listener {
 
 
     @EventHandler
     public void onPlayerHit(EntityDamageByEntityEvent e){
-        if (e.getDamager() instanceof Projectile && e.getDamager() instanceof Player) {
+        if (e.getDamager() instanceof Projectile || e.getDamager() instanceof Player) {
             Projectile a = (Projectile) e.getDamager();
             if (a.getShooter() instanceof Player){
                 Player shooter = (Player) a.getShooter();
-                if(shooter){
+                Player victim = (Player) e.getEntity();
+                if(redTeam.contains(shooter.getName()) && redTeam.contains(victim.getName())){
                     e.setCancelled(true);
                     shooter.sendMessage(ChatFormat.formatExclaim(ChatLevel.INFO, "Don't hurt your team!"));
+                } else if(blueTeam.contains(shooter.getName()) && blueTeam.contains(victim.getName())){
+                    e.setCancelled(true);
+                    shooter.sendMessage(ChatFormat.formatExclaim(ChatLevel.INFO, "Don't hurt your team!"));
+                } else {
+                    ;
                 }
             }
 
--- src/net/byteplex/ByteplexMatches/teams/TeamCommand.java
+++ src/net/byteplex/ByteplexMatches/teams/TeamCommand.java
@@ -8,7 +8,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Team;
 
 import static net.byteplex.ByteplexMatches.ByteplexMatches.blueTeam;
 import static net.byteplex.ByteplexMatches.ByteplexMatches.redTeam;
